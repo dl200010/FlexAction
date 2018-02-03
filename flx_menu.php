1b496e279b224c602eede3347757f31d55407f9e
DL200010 <dl200010@gmail.com>
2018-02-02 23:32:05 -0600

    temp menu and settings for the purpose of displaying functionality

---
+++ flx_menu.php
@@ -0,0 +1,7 @@
+<?php
+	//use this file to create the menu to be used in layout.php
+?>
+<ul>
+	<li><a href="?action=home.home">Home</a></li>
+	<li><a href="?action=test.home">Test</a></li>
+</ul>
--- flx_settings.php
+++ flx_settings.php
@@ -1,3 +1,4 @@
 <?php
 	//use this file to add settings
+	$flexaction['tempglobalsetting'] = "tempvalue";
 ?>
