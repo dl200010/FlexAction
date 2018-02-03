3da06f26fe0d1ebda018b6f7a6a3572136df7ecf
DL200010 <dl200010@gmail.com>
2018-02-02 21:24:44 -0600

    remove config lines from functions file

--- flx_functions.php
+++ flx_functions.php
@@ -1,7 +1,4 @@
 <?php
-	//this is the default action, in the case that it is missing from the URL
-	$flexaction['flx_action_empty'] = "home.home";
-
 	//here will be the functions part where each function will be mapped to a folder
 	$flexaction['functions']['home'] = "/";
 ?>
