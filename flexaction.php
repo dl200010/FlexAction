e917a25e1ac4ac74608e6cda1647a825a572ba78
DL200010 <dl200010@gmail.com>
2018-02-04 12:19:58 -0600

    add license and read me before putting into github

--- flexaction.php
+++ flexaction.php
@@ -1,4 +1,20 @@
 <?php
+	/*
+	 *  Copyright 2018 DL200010 <DL200010@gmail.com>
+	 *
+	 *  Licensed under the Apache License, Version 2.0 (the "License");
+	 *  you may not use this file except in compliance with the License.
+	 *  You may obtain a copy of the License at
+	 *
+	 *      http://www.apache.org/licenses/LICENSE-2.0
+	 *
+	 *  Unless required by applicable law or agreed to in writing, software
+	 *  distributed under the License is distributed on an "AS IS" BASIS,
+	 *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	 *  See the License for the specific language governing permissions and
+	 *  limitations under the License.
+	 */
+
 	//get the path that this file is in because everything else will deal off of this path.
 	$flexaction['flx_root_path'] = dirname(__FILE__);
 	if(file_exists('flx_config.php')) {
