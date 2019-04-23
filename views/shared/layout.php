4fdf469f0066fc91fb4d8bc20e6779f4adc3912a
Christopher Dangerfield <dl200010@gmail.com>
2019-04-23 13:14:08 -0500

    Completely remove HTML5UP template.

--- views/shared/layout.php
+++ views/shared/layout.php
@@ -14,44 +14,26 @@
 	 *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 	 *  See the License for the specific language governing permissions and
 	 *  limitations under the License.
-	 *
-	 *
-	 *
-	 *  Editorial by HTML5 UP
-	 *  html5up.net | @ajlkn
-	 *  Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 	 */
 ?>
 <html>
 	<head>
-		<title>Editorial by HTML5 UP</title>
+		<title></title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="/assets/css/main.css" />
 	</head>
 	<body>
 
-		<!-- Wrapper -->
-			<div id="wrapper">
+		<div id="wrapper">
 
-				<!-- Main -->
-					<div id="main">
-						<?php echo $flexaction['page_display']; ?>
-					</div>
-
-				<!-- Sidebar -->
-					<div id="sidebar">
-						<?php include 'menu.php'; ?>
-					</div>
-
+			<div id="main">
+				<?php echo $flexaction['page_display']; ?>
 			</div>
 
-		<!-- Scripts -->
-			<script src="/assets/js/jquery.min.js"></script>
-			<script src="/assets/js/browser.min.js"></script>
-			<script src="/assets/js/breakpoints.min.js"></script>
-			<script src="/assets/js/util.js"></script>
-			<script src="/assets/js/main.js"></script>
+			<div id="sidebar">
+				<?php include 'menu.php'; ?>
+			</div>
 
+		</div>
 	</body>
 </html>
